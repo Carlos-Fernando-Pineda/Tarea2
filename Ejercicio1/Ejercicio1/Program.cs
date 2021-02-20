9df9162392f46f3d24ef0534dd5deca27b0fc63c
Carlos-Fernando-Pineda <79342082+Carlos-Fernando-Pineda@users.noreply.github.com>
2021-02-19 19:31:58 -0600

    Add files via upload

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+    class Carlos
+    {
+        static void Main()
+        {
+            bool resultado;
+
+            char letra = 'B';
+
+            resultado = Char.IsUpper(letra);
+            Console.WriteLine(resultado);
+        }
+    }
+}
